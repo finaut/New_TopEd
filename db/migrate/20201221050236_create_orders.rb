d95ccc40a47aef3969d4ae52a8336868205b1dd2
finaut477@gmail.com <finaut477@gmail.com>
2020-12-21 10:28:58 -0800

    add colum status to table orders

--- db/migrate/20201221050236_create_orders.rb
+++ db/migrate/20201221050236_create_orders.rb
@@ -7,6 +7,7 @@ class CreateOrders < ActiveRecord::Migration[5.2]
       t.string :country
       t.string :faculty
       t.string :university
+      t.string :status
       t.references :user, foreign_key: true
 
       t.timestamps
