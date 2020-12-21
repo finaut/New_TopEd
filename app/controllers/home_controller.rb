b07bf8dc2e29f41637f85e78f51200646b8edfc6
finaut477@gmail.com <finaut477@gmail.com>
2020-12-21 10:20:01 -0800

    create static pages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
-  def index
-  end
+  def index; end
+  def company; end
+  def ukrain; end
+  def required_documents; end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_21_035927) do
-
-  create_table "comments", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.integer "university_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["university_id"], name: "index_comments_on_university_id"
-  end
+ActiveRecord::Schema.define(version: 2020_12_21_023932) do
 
   create_table "faculties", force: :cascade do |t|
     t.string "name"
