873ca415949fdcbf1b356fba75a9d94d0a1bc1ed
finaut477@gmail.com <finaut477@gmail.com>
2020-12-22 11:01:14 -0800

    add route service

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'home/company'
   get 'home/ukrain'
   get 'home/required_documents'
+  get 'home/service'
   resources :universities
   devise_for :users
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
