398fdb9f4618b682a1a4ead26b8973e294e23cc6
finaut477@gmail.com <finaut477@gmail.com>
2020-12-25 11:08:09 -0800

    add faculty

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'home/ukrain'
   get 'home/required_documents'
   get 'home/service'
+  resources :faculties
   resources :universities
   devise_for :users
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
