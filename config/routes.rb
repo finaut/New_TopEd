0dd50e8a0dd5ba7a8051002bc01f4fdc0abc368f
finaut477@gmail.com <finaut477@gmail.com>
2020-12-25 20:01:50 -0800

    indication of dependence routes of Institutes and faculties, users and orders

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,13 @@ Rails.application.routes.draw do
   get 'home/ukrain'
   get 'home/required_documents'
   get 'home/service'
-  resources :faculties
-  resources :universities
-  devise_for :users
+
+  resources :universities do
+    resources :comments
+  end
+  devise_for :users do
+    resources  :orders
+  end
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
