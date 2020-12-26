cdc40db7865e5f80ab073c21eb5374c2f97c0672
finaut477@gmail.com <finaut477@gmail.com>
2020-12-25 19:57:25 -0800

    added user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :first_name, :last_name, :mobile_number, :country, presence: true
+  validates :first_name, :last_name, length: { in: 3..25}
   has_many :orders
 end
