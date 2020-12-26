9ce9c35f1b8c5cf639f5d9bcfddac4aa621660a5
finaut477@gmail.com <finaut477@gmail.com>
2020-12-25 19:55:59 -0800

    added comment validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :university
+  validates :description, :name, presence: true
+  validates :name, length: { in: 3..20}
+  validates :description, length: { in: 10..255}
 end
