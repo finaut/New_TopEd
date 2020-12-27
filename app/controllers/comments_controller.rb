039645baa3f5e376abc1c620159a52354d1d49c6
finaut477@gmail.com <finaut477@gmail.com>
2020-12-26 17:48:41 -0800

    added error output for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,10 @@ class CommentsController < ApplicationController
   def create
     @university = University.find(params[:university_id])
     @comment = @university.comments.create set_params
+    if @comment.errors.any?
+      $comment_errors = @comment.errors
+    end
+    redirect_to university_path(@university)
   end
 
   private
