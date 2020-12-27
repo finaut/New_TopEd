2ee6faf8bcda9c9b4d345835f9576e4e18502b63
finaut477@gmail.com <finaut477@gmail.com>
2020-12-26 17:46:30 -0800

    Added the ability to comment to unregistered users

--- app/controllers/universities_controller.rb
+++ app/controllers/universities_controller.rb
@@ -1,82 +1,39 @@
 class UniversitiesController < ApplicationController
-  before_action :set_university, only: [:show, :edit, :update, :destroy]
+  before_action :set_university, only: [:show]
 
-  # GET /universities
-  # GET /universities.json
   def index
     @faculties = Faculty.all
     if params[:faculty_id]
-      @universities = University.where(faculties: params[:faculty_id])
+      @universities = find_universities
     else
       @universities = University.all
     end
   end
 
-
   def show
-    @user_id = current_user.id
-    @user = User.find(@user_id)
-    @orders = @user.orders.build
+    if current_user
+      @user_id = current_user.id
+      @user = User.find(@user_id)
+      @orders = @user.orders.build
+    end
     @comments = @university.comments.build
 
   end
 
-  # GET /universities/new
-  def new
-    @university = University.new
-  end
-
-  # GET /universities/1/edit
-  def edit
-  end
-
-  # POST /universities
-  # POST /universities.json
-  def create
-    @university = University.new(university_params)
-
-    respond_to do |format|
-      if @university.save
-        format.html { redirect_to @university, notice: 'University was successfully created.' }
-        format.json { render :show, status: :created, location: @university }
-      else
-        format.html { render :new }
-        format.json { render json: @university.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /universities/1
-  # PATCH/PUT /universities/1.json
-  def update
-    respond_to do |format|
-      if @university.update(university_params)
-        format.html { redirect_to @university, notice: 'University was successfully updated.' }
-        format.json { render :show, status: :ok, location: @university }
-      else
-        format.html { render :edit }
-        format.json { render json: @university.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /universities/1
-  # DELETE /universities/1.json
-  def destroy
-    @university.destroy
-    respond_to do |format|
-      format.html { redirect_to universities_url, notice: 'University was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
+    def  find_universities
+      @universities_faculties = FacultiesUniversity.where(faculty_id: params[:faculty_id])
+      @universities_ids = Array.new
+      @universities_faculties.each do |associaty|
+        @universities_ids << associaty.university_id
+      end
+      @universities = University.where(id: @universities_ids)
+    end
+
     def set_university
       @university = University.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def university_params
       params.require(:university).permit(:name, :description, :image, :location, :city, :url)
     end
